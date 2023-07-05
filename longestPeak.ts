e1c09e23d545c22b511a9b42c1c0e03c02acbf25
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-07-05 13:44:06 +0300

    monotonic array

--- longestPeak.ts
+++ longestPeak.ts
@@ -1,29 +1,28 @@
 export function longestPeak(array: number[]) {
-  // Write your code here.
-  let i = 1;
-  let longestPeak = 0;
-  while (i < array.length - 1) {
+  let finalLoguestPeak = 0;
+  let leftIdx = 0;
+  let rigthIdx = 0;
+
+  for (let i = 0; i < array.length - 1; i++) {
     const isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
 
-    if (!isPeak) {
-      i++;
-      continue;
+    if (isPeak) {
+      leftIdx = i - 2;
+      while (leftIdx > 0 && array[leftIdx] < array[leftIdx - 1]) {
+        leftIdx--;
+      }
+
+      rigthIdx = i + 2;
+      while (rigthIdx < array.length && array[rigthIdx] < array[rigthIdx - 1]) {
+        rigthIdx++;
+      }
     }
 
-    let leftIdx = i - 2;
-    while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]) {
-      leftIdx--;
-    }
-
-    let rightIdx = i + 2;
-    while (rightIdx <= array.length && array[rightIdx] < array[rightIdx - 1]) {
-      rightIdx++;
-    }
-
-    const currentLongestPeak = rightIdx - leftIdx - 1;
-    longestPeak = Math.max(currentLongestPeak, longestPeak);
-    i = rightIdx;
+    const currentLonguestPeak = rigthIdx - leftIdx - 1;
+    finalLoguestPeak = Math.max(currentLonguestPeak, finalLoguestPeak);
   }
 
-  return longestPeak;
+  console.log(finalLoguestPeak);
+
+  return finalLoguestPeak;
 }
---
+++ monotonicArray.ts
@@ -0,0 +1,20 @@
+export function monotonicArray(array: number[]) {
+  let direction = 0;
+  for (let i = 0; i < array.length; i++) {
+    const firstNum = array[i];
+    const secondNum = array[i + 1];
+
+    if (firstNum !== secondNum) {
+      if (direction === 0) {
+        direction = secondNum - firstNum;
+      }
+
+      if (direction > 0 && firstNum > secondNum) return false;
+      if (direction < 0 && firstNum < secondNum) return false;
+    }
+  }
+
+  return true;
+}
+
+export function monotonicArray2(array: number[]) {}
