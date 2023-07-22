cafc9a1ad8800f5e31c68ebc3bced28153e01438
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-07-22 16:43:39 +0300

    add arrayOfProduct other solution

--- arrayOfProducts.ts
+++ arrayOfProducts.ts
@@ -1,14 +1,38 @@
+// export function arrayOfProducts(array: number[]) {
+//   const productArray: number[] = [];
+//   for (let i = 0; i < array.length; i++) {
+//     let product = 1;
+//     for (let j = 0; j < array.length; j++) {
+//       if (i !== j) {
+//         product *= array[j];
+//       }
+//     }
+//     productArray.push(product);
+//   }
+
+//   return productArray;
+// }
+
 export function arrayOfProducts(array: number[]) {
-  const productArray: number[] = [];
+  const leftProductArr = new Array(array.length).fill(0);
+  const rigthProductArr = new Array(array.length).fill(0);
+  const products = new Array(array.length).fill(0);
+
+  let leftProduct = 1;
   for (let i = 0; i < array.length; i++) {
-    let product = 1;
-    for (let j = 0; j < array.length; j++) {
-      if (i !== j) {
-        product *= array[j];
-      }
-    }
-    productArray.push(product);
+    leftProductArr[i] = leftProduct;
+    leftProduct *= array[i];
   }
 
-  return productArray;
+  let rigthProduct = 1;
+  for (let i = array.length - 1; i >= 0; i--) {
+    rigthProductArr[i] = rigthProduct;
+    rigthProduct *= array[i];
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    products[i] = leftProductArr[i] * rigthProductArr[i];
+  }
+
+  return products;
 }
